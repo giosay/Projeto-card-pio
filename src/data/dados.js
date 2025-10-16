65763785e02a92f831c56c2a46d6268ef5f1168a
Giovanna <giosay@hotmail.com>
2025-10-16 08:56:58 -0300

    Craiando dados para ser consumido

---
+++ src/data/dados.js
@@ -0,0 +1,48 @@
+const menu =[
+    {
+        id:1,
+        titulo:"jogo-1",
+        categoria:"gamer",
+        preco:200.00,
+        img:'/src/assets/img.jpg',
+        descricao:"jogo top 1"
+
+        id:2,
+        titulo:"jogo-2",
+        categoria:"acao",
+        preco:200.00,
+        img:'/src/assets/img.jpg',
+        descricao:"jogo top 2"
+
+        id:3,
+        titulo:"jogo-3",
+        categoria:"rpg",
+        preco:200.00,
+        img:'/src/assets/img.jpg',
+        descricao:"jogo top 3"
+
+        id:4,
+        titulo:"jogo-4",
+        categoria:"aventura",
+        preco:200.00,
+        img:'/src/assets/img.jpg',
+        descricao:"jogo top 4"
+
+        id:5,
+        titulo:"jogo-5",
+        categoria:"acao",
+        preco:200.00,
+        img:'/src/assets/img.jpg',
+        descricao:"jogo top 5"
+
+        id:6,
+        titulo:"jogo-6",
+        categoria:"aventura",
+        preco:200.00,
+        img:'/src/assets/img.jpg',
+        descricao:"jogo top 6"
+
+    }
+]
+
+export default menu
